0ac888ad7c7156f9d887ec7979921b50641fc3ef
逻辑 <1045382156@qq.com>
2018-12-05 18:20:03 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import Vue from 'vue'
 import './plugins/axios'
 import './plugins/util'
-
-// import 语句必须位于最顶层，不可放置与条件判断中
-// ES6规范：import是在编译之前就已经确定是否导入
-import './plugins/mock'   //  mock数据
-
 import App from './App.vue'
 import store from './vuex'
 import router from './router'
